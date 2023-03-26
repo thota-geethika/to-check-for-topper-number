7032d3c5ab634c5d0a3897def120746fedc58695
thota-geethika <geetika.sree@m2pfintech.com>
2023-03-26 13:08:05 +0530

    Add. Included the logic to find whether the given number is probable topper number or not.

--- src/CheckForTopperNumber.java
+++ src/CheckForTopperNumber.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class CheckForTopperNumber {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Taking input from user;
+        String numberToCheck;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number to check whether it is a probable topper number or not: ");
+        numberToCheck = scan.next();
+
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for(int iterator=0;iterator<numberToCheck.length();iterator++)
+        {
+            int currentNumber = Character.getNumericValue(numberToCheck.charAt(iterator));
+            if(currentNumber%2 == 0)
+            {
+                evenSum += currentNumber;
+            }
+            else
+            {
+                oddSum += currentNumber;
+            }
+        }
+
+        //Checking if oddSum is equal to evenSum
+        if(oddSum == evenSum)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+
     }
 }
